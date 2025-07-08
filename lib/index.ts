0be036dc055b2ce25bd60357f25419e23b010f18
malloudev <malloudev@gmail.com>
2025-07-08 05:16:28 +0200

    Nevermind that

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,2 @@
 
 
-const enum OperationMode {
-   default_bot,
-   compilation
-}
-
-// This is used by the aetheris android app and will not matter for npmjs users
-// The last argument is being checked for a specific mode
-switch(process.argv.at(-1)) {
-   case(OperationMode.default_bot):
-      
-   break;
-   case(OperationMode.compilation):
-      
-   break;
-}
